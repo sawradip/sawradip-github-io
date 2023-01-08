4d93462b54fb67e39905093d1dfefc2bb80e6c04
sawradip <sawradip0@gmail.com>
2023-01-08 08:23:08 -1200

    Changed Metadata, Loading screen & Logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Sawradip sahag',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Sawradip Saha is an AI engineer who specializes in Deep Learning Models and Machine Learning tools',
+    siteUrl: 'https://sawradip.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@sawradip',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Sawradip Saha',
+        short_name: 'Sawradip saha',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
